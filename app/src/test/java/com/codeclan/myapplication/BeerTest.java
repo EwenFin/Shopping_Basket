aa44bbb6c14ce7d053b994cd313d968b0444a0f6
Ewen Carr <ewencarr@hotmail.com>
2017-03-31 13:27:33 +0100

    beer test added

--- app/src/test/java/com/codeclan/myapplication/BeerTest.java
+++ app/src/test/java/com/codeclan/myapplication/BeerTest.java
@@ -1,8 +1,28 @@
 package com.codeclan.myapplication;
 
-/**
- * Created by user on 31/03/2017.
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;/mport org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
 
 public class BeerTest {
-}
+    Beer beer;
+
+    @Before
+    public void before(){
+        beer = new Beer(699, false);
+    }
+
+    @Test
+    public void testMilkPrice(){
+        assertEquals(89, beer.getPrice());
+    }
+
+    @Test
+    public void testIsBogof(){
+        assertEquals(false, beer.isBogof());
+    }
+
