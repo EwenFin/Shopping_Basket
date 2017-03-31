b478f8ccda3ff59bec0b9c05c548df7fda2aadf3
Ewen Carr <ewencarr@hotmail.com>
2017-03-31 11:13:44 +0100

    tested customer names and loyalty cards

--- app/src/main/java/com/codeclan/myapplication/Customer.java
+++ app/src/main/java/com/codeclan/myapplication/Customer.java
@@ -2,4 +2,28 @@ package com.codeclan.myapplication;
 
 
 public class Customer {
+
+    public String name;
+    public boolean loyalty_card;
+
+    public Customer(String name){
+        this.name = name;
+        this.loyalty_card = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasLoyalty_card() {
+        return loyalty_card;
+    }
+
+    public void setLoyalty_card(boolean loyalty_card) {
+        this.loyalty_card = loyalty_card;
+    }
 }
--- app/src/test/java/com/codeclan/myapplication/CustomerTest.java
+++ app/src/test/java/com/codeclan/myapplication/CustomerTest.java
@@ -1,6 +1,32 @@
 package com.codeclan.myapplication;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 
 public class CustomerTest {
+    Customer customer1;
+    Customer customer2;
+
+    @Before
+    public void before(){
+        customer1 = new Customer("Ewen");
+        customer2 = new Customer("Bob");
+
+    }
+
+    @Test
+    public void testName(){
+        assertEquals("Ewen", customer1.getName());
+    }
+
+    @Test
+    public void testStartWithoutLoyaltyCard(){
+        assertEquals(false, customer1.hasLoyalty_card());
+    }
+
+
 }
