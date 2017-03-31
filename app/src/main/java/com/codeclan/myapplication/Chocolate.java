d8043383afa3a7004d452ee4ba8e76e46c66fd2a
Ewen Carr <ewencarr@hotmail.com>
2017-03-31 13:25:46 +0100

    chocolate test passing

--- app/src/main/java/com/codeclan/myapplication/Chocolate.java
+++ app/src/main/java/com/codeclan/myapplication/Chocolate.java
@@ -1,8 +1,31 @@
 package com.codeclan.myapplication;
 
-/**
- * Created by user on 31/03/2017.
- */
 
-public class Chocolate {
+
+public class Chocolate implements Purchasables {
+
+    public int price;
+    public boolean bogof;
+
+    public Chocolate(int price, boolean bogof){
+        this.price = price;
+        this.bogof = bogof;
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isBogof() {
+        return bogof;
+    }
+
+    public void setBogof(boolean bogof) {
+        this.bogof = bogof;
+    }
 }
--- app/src/test/java/com/codeclan/myapplication/ChocolateTest.java
+++ app/src/test/java/com/codeclan/myapplication/ChocolateTest.java
@@ -1,6 +1,28 @@
 package com.codeclan.myapplication;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
 
 public class ChocolateTest {
+    Chocolate chocolate;
+
+    @Before
+    public void before(){
+        chocolate = new Chocolate(100, false);
+    }
+
+    @Test
+    public void testMilkPrice(){
+        assertEquals(100, chocolate.getPrice());
+    }
+
+    @Test
+    public void testIsBogof() {
+        assertEquals(false, chocolate.isBogof());
+    }
+
+
 }
